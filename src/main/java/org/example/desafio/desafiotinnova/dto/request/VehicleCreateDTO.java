412cacec60bb4c49ac153360165e6dae2e8fa0b4
ivinib <vi_ni_b@hotmail.com>
2026-06-29 09:22:04 -0300

    Creating vehicle request and response DTOs

---
+++ src/main/java/org/example/desafio/desafiotinnova/dto/request/VehicleCreateDTO.java
@@ -0,0 +1,29 @@
+package org.example.desafio.desafiotinnova.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Min;
+
+import java.math.BigDecimal;
+
+public record VehicleCreateDTO (
+    @NotBlank(message = "Licende plate is required")
+    @Pattern(regexp = "^[A-Z]{3}[0-9][A-Z0-9][0-9]{2}$", message = "Licence place format must be AAAA-1111 or AAA-1A11")
+    String licencePlate,
+
+    @NotBlank(message = "Vehicle brand is required")
+    String brand,
+
+    @NotNull(message = "Vehicle year is required")
+    @Min(value = 1900, message = "Year must be greater than 1900")
+    Integer year,
+
+    @NotBlank(message = "Vehicle color is required")
+    String color,
+
+    @NotNull(message = "Vehicle price in Brazilian Real currency is required")
+    @Positive(message = "Price must be greater than zero")
+    BigDecimal price
+){}
---
+++ src/main/java/org/example/desafio/desafiotinnova/dto/response/VehicleResponseDTO.java
@@ -0,0 +1,14 @@
+package org.example.desafio.desafiotinnova.dto.response;
+
+import java.math.BigDecimal;
+
+public record VehicleResponseDTO(
+        Long idVehicle,
+        String licencePlate,
+        String brand,
+        Integer year,
+        String color,
+        BigDecimal priceInUSD,
+        BigDecimal priceInBRL,
+        BigDecimal currency
+) {}
