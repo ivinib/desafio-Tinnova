54cf942683fb924da5b16910e814a144d7da33c6
ivinib <vi_ni_b@hotmail.com>
2026-06-29 08:16:37 -0300

    Initial commit

---
+++ src/test/java/org/example/desafio/desafiotinnova/DesafioTinnovaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.desafio.desafiotinnova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTinnovaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
